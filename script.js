62de147fc8eddcf3fff68f5877fc81bdc3f18635
Kelvin Raharja <kelvin.raharja@22squared.com>
2018-08-21 12:19:17 -0400

    trying to use HTTPS for openweathermap api and using zip code

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-window.onload = function() {
-
-	// Check to see if the browser supports the GeoLocation API.
-	if (navigator.geolocation) {
-		// Get the location
-		navigator.geolocation.getCurrentPosition(function(position) {
-			var lat = position.coords.latitude;
-			var lon = position.coords.longitude;
-			console.log('latitude: ' + lat);
-			console.log('longitude: ' + lon);
-
-			// Show the JSON
-			gettingJSON(lat, lon);
-		});
-	} else {
-		// Print out a message to the user.
-		document.write('Your browser does not support GeoLocation :(');
-	}
-
-}
+// window.onload = function() {
+//
+// 	// Check to see if the browser supports the GeoLocation API.
+// 	if (navigator.geolocation) {
+// 		// Get the location
+// 		navigator.geolocation.getCurrentPosition(function(position) {
+// 			var lat = position.coords.latitude;
+// 			var lon = position.coords.longitude;
+// 			console.log('latitude: ' + lat);
+// 			console.log('longitude: ' + lon);
+//
+// 			// Show the JSON
+// 			gettingJSON(lat, lon);
+// 		});
+// 	} else {
+// 		// Print out a message to the user.
+// 		document.write('Your browser does not support GeoLocation :(');
+// 	}
+//
+// }
 
 // get OpenWeather API JSON
-function gettingJSON(lat, lon){
+function gettingJSON(zip){
 		// document.write("jquery loaded");
-		$.getJSON("https://crossorigin.me/https://api.darksky.net/forecast/09ffec8ffd7df43c9e4ec34117d734f7/" + lat + "," + lon,function(json){
+		var x = document.getElementById("zipvalue");
+    var zip = "";
+    zip += x.value;
+		console.log('zip entered: ' + zip);
+		$.getJSON("api.openweathermap.org/data/2.5/weather?zip=" + zip + "&appid=d78f5363c58b1e99ac14e3786c1b25ae",function(json){
 				document.write(JSON.stringify(json));
 				showForecast();
 		});
