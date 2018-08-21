1d1d6261adba9bf68e704fb0786b72a3e8d50b6e
Kelvin Raharja <kelvin.raharja@22squared.com>
2018-08-21 16:45:47 -0400

    added option to use geolocation or zip code.

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 window.onload = function() {
 
-	// variable to store weather json data
-	var jsonData;
+	// global variables
+	var lat, lon, jsonData;
 
-	// form click button
-	$("#zipform").submit(function (event) {
+	// clear button
+	$("#clearBtn").on('click', function (event) {
 		event.preventDefault();
-		var zip = $('#zipvalue').value;
+		$('#jsondata').text('');
 		console.log('zip entered: ' + zip);
 	});
 
-	// Check to see if the browser supports the GeoLocation API.
-	if (navigator.geolocation) {
-		// Get the location
-		navigator.geolocation.getCurrentPosition(function(position) {
-			var lat = position.coords.latitude;
-			var lon = position.coords.longitude;
-			console.log('latitude: ' + lat);
-			console.log('longitude: ' + lon);
+	// zip form button
+	$("#zipform").submit(function (event) {
+		event.preventDefault();
+		var zip = $('#zipvalue').val();
+		console.log('zip entered: ' + zip);
+		var coor = getLatLngByZipcode(zip);
+		gettingJSON(coor[0], coor[1]);
+	});
 
-			// Show the JSON
-			gettingJSON(lat, lon);
-		});
-	} else {
-		// Print out a message to the user.
-		document.write('Your browser does not support GeoLocation :(');
-	}
+	// geolocation button
+	$("#geoLocBtn").on('click', function (event) {
+		console.log('geolocation requested');
+		event.preventDefault();
+		// Check to see if the browser supports the GeoLocation API.
+		if (navigator.geolocation) {
+			// Get the location
+			navigator.geolocation.getCurrentPosition(function (position) {
+				lat = position.coords.latitude;
+				lon = position.coords.longitude;
+				console.log('latitude: ' + lat);
+				console.log('longitude: ' + lon);
 
-	// get OpenWeather API JSON
+				// Show the JSON
+				gettingJSON(lat, lon);
+			});
+		} else {
+			// Print out a message to the user.
+			document.write('Your browser does not support GeoLocation :(');
+		}
+	});
+
+	// get OpenWeather API JSON using latitude and longitude
 	function gettingJSON(lat, lon) {
 		// document.write("jquery loaded");
 		$.getJSON("https://api.openweathermap.org/data/2.5/forecast?appid=d78f5363c58b1e99ac14e3786c1b25ae&lat=" + lat + "&lon=" + lon, function (json) {
@@ -43,4 +57,19 @@ window.onload = function() {
 		$('#jsondata').text(jsonData);
 	};
 
+	// Get geo coordinates from zip code
+	function getLatLngByZipcode(zipcode) {
+		var geocoder = new google.maps.Geocoder();
+		var address = zipcode;
+		geocoder.geocode({ 'address': address }, function (results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				var latitude = results[0].geometry.location.lat();
+				var longitude = results[0].geometry.location.lng();
+				alert("Latitude: " + latitude + "\nLongitude: " + longitude);
+			} else {
+				alert("Request failed.")
+			}
+		});
+		return [latitude, longitude];
+	}
 }
